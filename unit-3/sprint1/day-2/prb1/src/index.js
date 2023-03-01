58cfb0a66c38048be4fceab1cda3d10a1a19cd6d
Vaibhav Panwar <p.vaibhav7499@gmail.com>
2023-03-01 14:35:30 +0530

    day2

--- unit-3/sprint1/day-2/prb1/src/index.js
+++ unit-3/sprint1/day-2/prb1/src/index.js
@@ -1,5 +1,25 @@
-function studentData() {
-  return {}
+function studentData(fname,lname,age,marks,...hobbies) {
+  return {
+    fullName : `${fname} ${lname}`,
+    age,
+    hobbies,
+    getInfo(){
+    return `${fname} ${lname}'s age is ${age}.`},
+    getResult() {
+    let sum = 0;
+    for (let i = 0; i < marks.length; i++) {
+      sum = sum + marks[i];
+    }
+    let avg = sum / marks.length;
+    if (avg < 50) {
+      return `Result: FAIL`;
+    }
+    else {
+      return `Result: PASS`;
+    }
+  }
+  }
+
 }
 
 export {
