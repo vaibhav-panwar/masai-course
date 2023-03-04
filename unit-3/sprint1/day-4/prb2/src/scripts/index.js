7a026fe14cc25aeb243ef1e3813e837ff15bf68f
Vaibhav Panwar <p.vaibhav7499@gmail.com>
2023-03-04 17:10:32 +0530

    d4p2

---
+++ unit-3/sprint1/day-4/prb2/src/scripts/index.js
@@ -0,0 +1,43 @@
+let elform = document.getElementById("mobile_form");
+let elname = document.getElementById("mobile_name");
+let elbrand = document.getElementById("mobile_brand");
+let elprice = document.getElementById("mobile_price");
+let elimage = document.getElementById("mobile_image");
+console.log(elform);
+elform.addEventListener("submit",(e)=>{
+  e.preventDefault();
+  getInputData();
+  elname.value = ""
+  elbrand.value = ""
+  elprice.value = ""
+  elimage.value = ""
+});
+function getInputData(){
+  let name = elname.value;
+  let brand = elbrand.value;
+  let price = elprice.value;
+  let image = elimage.value;
+  addData(name,brand,price,image);
+}
+
+function addData(name, brand, price, image) {
+   let arr = JSON.parse(localStorage.getItem("mobile_data"));
+   if(arr==null){
+    arr = [];
+   }
+   let obj ={
+    name,
+    brand,
+    price,
+    image
+   }
+   arr.push(obj);
+   localStorage.setItem("mobile_data",JSON.stringify(arr));
+}
+
+// do not change this
+if (typeof exports !== "undefined") {
+  module.exports = {
+    addData,
+  };
+}
---
+++ unit-3/sprint1/day-4/prb2/src/scripts/store.js
@@ -0,0 +1,71 @@
+let arr = JSON.parse(localStorage.getItem("mobile_data"));
+if(arr == null){
+  arr =[];
+}
+let eldiv = document.getElementById("mobile_list");
+let btn1 = document.getElementById("btn1");
+let btn2 = document.getElementById("btn2");
+btn1.addEventListener("click",()=>{
+  sortLowToHigh();
+})
+btn2.addEventListener("click",()=>{
+  sortHighToLow();
+})
+append(arr);
+function append(data) {
+  eldiv.innerHTML = "";
+  data.forEach((el,i)=>{
+    let div = document.createElement("div");
+    let img = document.createElement("img");
+    img.setAttribute("src",el.image);
+    img.setAttribute("alt","error");
+    let name = document.createElement("h3");
+    name.innerText = el.name;
+    let brand = document.createElement("h3");
+    brand.innerText = el.brand;
+    let price = document.createElement("h3");
+    price.innerText = el.price;
+    let btn = document.createElement("button");
+    btn.innerText = "Remove";
+    btn.addEventListener("click",()=>{
+      remove(i);
+    })
+    div.append(img,name,brand,price,btn);
+    eldiv.append(div); 
+  })
+}
+
+function remove(index) {
+  arr = arr.filter((el,ind)=>{
+    if(ind==index){
+      return false;
+    }
+    else{
+      return true;
+    }
+  })
+  localStorage.setItem("mobile_data",JSON.stringify(arr));
+  append(arr);
+}
+
+function sortLowToHigh() {
+  // sort the list of mobiles in ascendning order of price
+  arr = arr.sort(function(a,b){return a.price - b.price})
+  append(arr)
+}
+
+function sortHighToLow() {
+  // sort the list of mobiles in descending order of the price
+  arr = arr.sort(function (a, b) { return b.price - a.price })
+  append(arr)
+}
+
+// do not change this
+if (typeof exports !== "undefined") {
+  module.exports = {
+    append,
+    remove,
+    sortLowToHigh,
+    sortHighToLow,
+  };
+}
