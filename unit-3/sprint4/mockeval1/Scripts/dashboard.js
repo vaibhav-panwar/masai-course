29bad409faf1b217db4a9e66f6f46c2e8197a77c
Vaibhav Panwar <p.vaibhav7499@gmail.com>
2023-03-22 21:01:47 +0530

    try3

--- unit-3/sprint4/mockeval1/Scripts/dashboard.js
+++ unit-3/sprint4/mockeval1/Scripts/dashboard.js
@@ -1,5 +1,5 @@
 let dArr = JSON.parse(localStorage.getItem("assignments"));
-if(dArr==null){
+if (dArr == null) {
     dArr = [];
 }
 let sprint1 = document.getElementById("sprint-1");
@@ -7,35 +7,35 @@ let sprint2 = document.getElementById("sprint-2");
 let sprint3 = document.getElementById("sprint-3");
 let sprint4 = document.getElementById("sprint-4");
 let scourse = document.getElementById("course");
-scourse.addEventListener("change",()=>{
+scourse.addEventListener("change", () => {
     let a = scourse.value;
     sprint1.innerHTML = "";
     sprint2.innerHTML = "";
     sprint3.innerHTML = "";
     sprint4.innerHTML = "";
-    display(a);
+    display(a, dArr);
 })
 
 
-display("JS101",dArr)
-function display(course,arr){
-  let filtered= arr.filter((e)=>{
-    if(e.course==course){
-        return true
-    }
-    else{
-        return false
-    }
-  })
-    filtered.forEach((e)=>{
+display("JS101", dArr)
+function display(course, arr) {
+    let filtered = arr.filter((e) => {
+        if (e.course == course) {
+            return true
+        }
+        else {
+            return false
+        }
+    })
+    filtered.forEach((e) => {
         displayAss(e.name, e.desc, e.course, e.type, e.schedule, e.sprint);
     })
 }
-function displayAss(name,desc,course,type,schedule,sprint){
+function displayAss(name, desc, course, type, schedule, sprint) {
     let div = document.createElement("div");
     let p1 = document.createElement("p");
     p1.textContent = name;
-    let p2  =document.createElement("p");
+    let p2 = document.createElement("p");
     p2.textContent = desc;
     let p3 = document.createElement("p");
     p3.textContent = type;
@@ -48,7 +48,7 @@ function displayAss(name,desc,course,type,schedule,sprint){
     let select = document.createElement("select");
     let opt1 = document.createElement("option");
     opt1.textContent = "sprint-1";
-    opt1.setAttribute("value","sprint-1")
+    opt1.setAttribute("value", "sprint-1")
     let opt2 = document.createElement("option");
     opt2.textContent = "sprint-2";
     opt2.setAttribute("value", "sprint-2")
@@ -58,26 +58,31 @@ function displayAss(name,desc,course,type,schedule,sprint){
     let opt4 = document.createElement("option");
     opt4.textContent = "sprint-4";
     opt4.setAttribute("value", "sprint-4");
-    select.append(opt1,opt2,opt3,opt4);
-    select.addEventListener("change",()=>{
-     let a = select.value;
-     intosprint(a,div);
-     dArr.forEach((el)=>{
-        if(el.name==name && el.desc==desc && el.course == course && el.type==type && el.schedule == schedule){
-            el.sprint = a;
-        }
-     })
-     localStorage.setItem("assignments",JSON.stringify(dArr));
+    select.append(opt1, opt2, opt3, opt4);
+    select.addEventListener("change", (e) => {
+        e.preventDefault();
+        let a = select.value;
+        dArr.forEach((el) => {
+            if (el.name == name && el.desc == desc && el.course == course && el.type == type && el.schedule == schedule) {
+                el.sprint = a;
+            }
+        })
+        localStorage.setItem("assignments", JSON.stringify(dArr));
+        sprint1.innerHTML = "";
+        sprint2.innerHTML = "";
+        sprint3.innerHTML = "";
+        sprint4.innerHTML = "";
+        display(course, dArr);
     })
     let btn = document.createElement("button");
-    btn.textContent= "Delete";
-    btn.addEventListener("click",(e)=>{
+    btn.textContent = "Delete";
+    btn.addEventListener("click", (e) => {
         e.preventDefault();
-        dArr = dArr.filter((el)=>{
-            if(el.name ==name && el.course == course && el.sprint ==sprint && el.desc ==desc && el.type ==type && el.schedule==schedule){
+        dArr = dArr.filter((el) => {
+            if (el.name == name && el.course == course && el.sprint == sprint && el.desc == desc && el.type == type && el.schedule == schedule) {
                 return false;
             }
-            else{
+            else {
                 return true;
             }
         })
@@ -87,22 +92,22 @@ function displayAss(name,desc,course,type,schedule,sprint){
         sprint2.innerHTML = "";
         sprint3.innerHTML = "";
         sprint4.innerHTML = "";
-        display(course,dArr);
-        
+        display(course, dArr);
+
     })
-    div.append(p1,p2,p3,p4,p5,p6,select,btn);
-    intosprint(sprint,div);
+    div.append(p1, p2, p3, p4, p5, p6, select, btn);
+    intosprint(sprint, div);
 }
-function intosprint(sprint,div){
+function intosprint(sprint, div) {
     if (sprint == "sprint-1") {
         sprint1.append(div);
     }
     else if (sprint == "sprint-2") {
-        
+
         sprint2.append(div);
     }
     else if (sprint == "sprint-3") {
-        
+
         sprint3.append(div);
     }
     else if (sprint == "sprint-4") {
