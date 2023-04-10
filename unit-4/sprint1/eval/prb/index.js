c85e4b471ba1541b6e82a0a437cdca5329f7eef8
Vaibhav Panwar <p.vaibhav7499@gmail.com>
2023-04-10 16:42:55 +0530

    t2

--- unit-4/sprint1/eval/prb/index.js
+++ unit-4/sprint1/eval/prb/index.js
@@ -11,25 +11,32 @@ var yodasay = require('yodasay');
 let server = http.createServer((req, res) => {
     if (req.url == "/") {
         res.setHeader('content-type', 'text/html');
-        res.end = "<h1>Welcome to the Home Page</h1>"
+        res.end("<h1>Welcome to the Home Page</h1>")
     }
     else if (req.url == "/gencount") {
         fs.readFile('./data.json', "utf-8", (err, data) => {
             if (err) {
-                res.end = err
+                res.end(err)
             }
             else {
                 let m = 0;
                 let f = 0;
-                data.forEach((el) => {
-                    if (el.gender == "male") {
+                for(let i=0;i<data.length;i++){
+                    if(data[i].gender=="male"){
                         m++;
                     }
-                    else if (el.gender == "female") {
+                    else if (data[i].gender=="female"){
                         f++;
                     }
+                }
+                fs.appendFile("./logs.txt", `The initial Male count is ${m} and Female count is ${f} at ${new Date()}`,(err)=>{
+                    if(err){
+                        res.end(err);
+                    }
+                    else{
+                        res.end("The count has been updated in the logs file");
+                    }
                 })
-                fs.appendFile("./logs.txt", `The initial Male count is ${m} and Female count is ${f} at ${new Date()}`)
             }
         })
     }
@@ -42,36 +49,34 @@ let server = http.createServer((req, res) => {
           "gender" : "female"
         }`, (err) => {
             if (err) {
-                res.end = err;
+                res.end(err);
             }
             else {
-                res.end = "The data has been updated, go and check the data file"
+                res.end("The data has been updated, go and check the data file")
             }
         })
     }
     else if (req.url == "/people") {
-        try {
-            let a = fs.readFileSync('./data.json', "utf-8");
+
+            let a = fs.readFile('./data.json', "utf-8");
             a.forEach((el) => {
-                fs.appendFileSync('./people.txt', `\n First Name: ${el.first_name} Last Name: ${el.last_name} Gender: ${el.gender} Email: ${el.email}`);
+                fs.appendFile('./people.txt', `\n First Name: ${el.first_name} Last Name: ${el.last_name} Gender: ${el.gender} Email: ${el.email}`);
             })
-            res.end = "The file has been created and data has been entered"
-        } catch (error) {
-            res.end = error
-        }
+            res.end("The file has been created and data has been entered")
+        
     }
     else if (req.url == "/address") {
         dns.lookup("masaischool.com", (err, address, family) => {
             if (err) {
-                res.end = err;
+                res.end(err);
             }
             else {
                 fs.appendFile('./logs.txt', `URL: masaischool.com IP Address: ${address} and Family is IPv${family}`, (err) => {
                     if (err) {
-                        res.end = err;
+                        res.end(err);
                     }
                     else {
-                        res.end = "Logs File has been updated";
+                        res.end("Logs File has been updated");
                     }
                 })
             }
@@ -79,14 +84,18 @@ let server = http.createServer((req, res) => {
     }
     else if (req.url == "./yoda") {
         let a = fs.readFileSync("./people.txt", "utf-8");
-        res.end = yodasay.say({
+        res.end(yodasay.say({
             text: a
-        })
+        }))
     }
 
 })
 
 
+
+// server.listen(5500,()=>{
+//     console.log("working");
+// })
 
 
   module.exports = server;
