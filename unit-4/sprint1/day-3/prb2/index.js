97c01425d39c87cea6272c54c326cbb9682e6dea
Vaibhav Panwar <p.vaibhav7499@gmail.com>
2023-04-11 20:15:46 +0530

    prb2

---
+++ unit-4/sprint1/day-3/prb2/index.js
@@ -0,0 +1,53 @@
+const http = require("http");
+const fs = require("fs");
+const PORT = 7700;
+// let a = fs.readdirSync("C:/Users/admin/Desktop/masai-course/unit-4/sprint1/day-3/prb2");
+// for (let i = 0; i < a.length; i++) {
+//         let b = fs.statSync(`C:/Users/admin/Desktop/masai-course/unit-4/sprint1/day-3/prb2/${a[i]}`);
+
+//         if (b.isDirectory()) {
+//             let temp = fs.readdirSync(`C:/Users/admin/Desktop/masai-course/unit-4/sprint1/day-3/prb2/${a[i]}`);
+//             for (let j = 0; j < temp.length; j++) {
+//                 console.log(a[i],temp[j]);
+//             }
+//             console.log("end---------------------------------------------------------------------")
+//         }
+       
+//     }
+
+const server = http.createServer((req, res) => {
+    let link = "C:/Users/admin/Desktop/masai-course/unit-4/sprint1/day-3/prb2";
+    let a = fs.readdirSync(link);
+    res.setHeader('content-type', 'text/html')
+    if (req.url == "/") {
+        for (let i = 0; i < a.length; i++) {
+            res.write(`<li><a href="http://localhost:${PORT}/${a[i]}">${a[i]}</a></li>`)
+        }
+        return res.end();
+    }
+    else{
+        for (let i = 0; i < a.length; i++) {
+            if (req.url == `/${a[i]}`) {
+                let b = fs.statSync(`${link}/${a[i]}`);
+                if (b.isDirectory()) {
+                    let temp = fs.readdirSync(`${link}/${a[i]}`);
+                    for (let j = 0; j < temp.length; j++) {
+                        res.write(`<li><a href="http://localhost:${PORT}/${a[i]}/${temp[j]}">${temp[j]}</a></li>`)
+                    }
+                    return res.end()
+                }
+                else if(b.isFile()){
+                    let read = fs.readFileSync(`${a[i]}`,"utf-8");
+                    res.end(JSON.stringify(read));
+                }
+            }
+        }
+        res.end("404 Not Found");
+    }
+    
+    
+}
+);
+// server.listen(PORT);
+// export your server
+module.exports = server;
