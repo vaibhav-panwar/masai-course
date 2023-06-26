8197e7768dc18a67f210ca0006c4fdbd8159bf43
vaibhav-panwar <119409806+vaibhav-panwar@users.noreply.github.com>
2023-06-26 16:21:15 +0530

    Delete unit-6/sprint-1/eval directory

--- unit-6/sprint-1/eval/models/department.js
+++
@@ -1,7 +0,0 @@
-module.exports = (sequelize,dataType)=>{
-    const Department = sequelize.define("departments",{
-        title:dataType.STRING,
-        description:dataType.STRING
-    })
-    return Department
-}
--- unit-6/sprint-1/eval/models/employee.js
+++
@@ -1,9 +0,0 @@
-module.exports = (sequelize,dataType)=>{
-    let Employee = sequelize.define("employees",{
-        Name:dataType.STRING,
-        Email:{type:dataType.STRING , allowNull:false},
-        Salary:dataType.INTEGER,
-        DepartmentID:dataType.INTEGER
-    })
-    return Employee
-}
