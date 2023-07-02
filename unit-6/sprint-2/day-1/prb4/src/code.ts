501a24176bd87e187675b5f4fd2171e4452ed399
Vaibhav Panwar <p.vaibhav7499@gmail.com>
2023-07-02 23:02:08 +0530

    prb4

---
+++ unit-6/sprint-2/day-1/prb4/src/code.ts
@@ -0,0 +1,11 @@
+let Obj :{
+title:string;
+status:boolean;
+id:number
+}={
+    title:"sdsd",
+    status:true,
+    id:233
+}// This Obj variable should have the interface that you will create
+
+export default Obj;// Make no changes here
