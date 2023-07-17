41017cd2621305a57eb5f68ff8ec7787eb44354d
Vaibhav Panwar <p.vaibhav7499@gmail.com>
2023-07-17 13:56:32 +0530

    Ds

--- unit-6/sprint-4/evaluation/eval-prb/src/Components/Genere.jsx
+++ unit-6/sprint-4/evaluation/eval-prb/src/Components/Genere.jsx
@@ -4,7 +4,7 @@ function Genere({arr}) {
   return <div data-test-id="genere-chips">
     {
       arr.map((el,id)=>{
-        return <p key={id}>{el}</p>
+        return <div key={id}>{el}</div>
       })
     }
   </div>;
